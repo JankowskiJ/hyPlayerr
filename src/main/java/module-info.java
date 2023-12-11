640843f264fc02e91553dc76e69cf79fff6920a8
Jakub Jankowski <jankowskijakub59@gamil.com>
2023-12-11 23:48:49 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.hyplayer {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires  java.sql;
+    requires  mysql.connector.j;
+
+    opens com.example.hyplayer to javafx.fxml;
+    exports com.example.hyplayer;
+}
